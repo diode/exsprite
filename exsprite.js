aba4abd6cdea0a9ba1877a45519bab52d7745149
diode <vipin@deepdio.com>
2011-06-13 13:19:26 +0530

    added transformations

--- exsprite.js
+++ exsprite.js
@@ -1,15 +1,14 @@
 /**
  * exSprite JavaScript Library v1.0.0
  * http://www.exsprite.com/
- * Copyright 2011, Eric Rowell
+ * Copyright 2011, Vipin V
  * Licensed under the MIT or GPL Version 2 licenses.
  * Date: Jun 12 2011
  *
- * Copyright 2011, Vipin V
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
+ * of this software to deal in the Software without
+ * restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
